f987482dbda00f8b637193312e0a8fd0f7316d04
tianyi <793187547@qq.com>
2019-04-03 15:39:03 +0800

    add config center

--- src/main/java/com/ty/demo/CloudApplication.java
+++ src/main/java/com/ty/demo/CloudApplication.java
@@ -3,6 +3,7 @@ package com.ty.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * 启动类
@@ -12,6 +13,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableConfigServer
 public class CloudApplication {
 
     public static void main(String[] args) {
